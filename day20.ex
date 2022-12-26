2083a51e10de387186339f3d771b40ee83ac3c38
Fredrik Hagman <fhagman@spotify.com>
2022-12-26 13:42:31 +0100

    day20

--- day20.ex
+++ day20.ex
@@ -1,67 +1,67 @@
 defmodule Day20 do
-  def input do
+  def input(f) do
     File.read!("input20.txt")
     |> String.split("\n")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(fn x -> String.to_integer(x) * f end)
     |> Enum.with_index
   end
 
-  def result(r) do
-    r
-    |> Enum.sort(fn {_,a}, {_,b} -> a<b end)
-    |> Enum.map(fn {n,_} -> n end)
+  def move(list, i, ni, a..b, o) do
+    list
+    |> Enum.map(fn {v, oi} ->
+      cond do
+        oi == i -> {v,ni}
+        oi >= a && oi <= b -> {v, oi + o}
+        true -> {v,oi}
+      end
+    end)
   end
-  def solve(sequense), do: solve(sequense, sequense)
-  def solve([], result), do: result(result)
-  def solve([{n,i}|s], result) do
-#    IO.inspect({n, result(result)})
-    {_, ix} = Enum.at(result, i)
-    l = Enum.count(result)
-#    IO.inspect({moveRange(ix, n, l)})
-    ni = choose(l <= ix+n, rem(ix+n+l+1,l), choose(n < 0, rem(ix+n+l-1,l), ix+n))
-    mr = MapSet.new(moveRange(ix, n, l))
-    solve(s, result
-    |> Enum.map(fn {nn,ii} -> move(nn, ii, n, ix, l, ni, mr) end))
-  end
-  def choose(true, a, _), do: a
-  def choose(_, _, a), do: a
-  def move(nn, ii, n, i, l, ni, mr) do
-#    IO.inspect({nn, ii, n, i, l})
-#    ni = choose(l <= i+n, rem(i+n+l+1,l), choose(n < 0, rem(i+n+l-1,l), i+n))
-#    mr = MapSet.new(moveRange(i, n, l))
-#    IO.inspect({mr, MapSet.member?(mr, ii)})
+
+  def adj(v) do
     cond do
-      n == 0 -> {nn, ii}
-      ii == i -> {nn, ni}
-      ni > i && MapSet.member?(mr, ii) -> {nn, ii - 1}
-      ni < i && MapSet.member?(mr, ii) -> {nn, ii + 1}
-      true -> {nn, ii}
-    end
-  end
-  def moveRange(i, n, l) do
-    cond do
-      n < 0 -> Enum.to_list(i..rem(i+n+l-1,l))
-      l <= i+n -> Enum.to_list(i..rem(i+n+l+1,l))
-      true -> Enum.to_list(i..rem(i+n+l,l))
+      v < 0 -> -1
+      true -> 0
     end
   end
 
-  def zeroIndex(l) do
+  def move(list, n, len) do
+    {v,i} = Enum.at(list, n)
+    ni = rem(rem(i + v, len-1) + len-1, len-1)
+    cond do
+      ni > i -> move(list, i, ni, i+1..ni, -1)
+      ni < i -> move(list, i, ni, ni..i-1, 1)
+      true -> list
+    end
+  end
+
+  def result(l) do
     l
-    |> Enum.with_index
-    |> Enum.filter(fn {n,_} -> n == 0 end)
-    |> Enum.at(0)
-    |> elem(1)
+    |> Enum.sort(fn {_,i1}, {_,i2} -> i1 < i2 end)
+    |> Enum.map(fn {x,_} -> x end)
+  end
+
+  def scramble(list, len) do
+    0..len-1
+    |> Enum.reduce(list, fn i, l -> move(l, i, len) end)
+  end
+
+  def solve(list, c) do
+    len = Enum.count(list)
+    nl = 1..c
+    |> Enum.reduce(list, fn _, l -> scramble(l, len) end)
+    |> Enum.sort(fn {_,i1}, {_,i2} -> i1 < i2 end)
+    si =  nl
+    |> Map.new
+    |> Map.fetch!(0)
+    [1000, 2000, 3000]
+    |> Enum.reduce(0, fn n, a -> a + elem(Enum.at(nl, rem(si + n, len)),0) end)
   end
 
   def main do
-    r = solve(input())
-    l = Enum.count(r)
-    zi = zeroIndex(r)
-    Enum.at(r, rem(1000 + zi, l)) + Enum.at(r, rem(2000 + zi, l)) + Enum.at(r, rem(3000 + zi, l))
-#    IO.inspect(solve(input()))
+    IO.inspect(solve(input(1),1))
+    IO.inspect(solve(input(811589153),10))
   end
 end
 
-# 69206
-# 197400
+# 3473
+# 7496649006261
